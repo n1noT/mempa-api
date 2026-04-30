a3d3476256ea13995c8cc401c8bde661dff54d82
Antoine Delagnes <antoine.delagnes@etu.iut-tlse3.fr>
2026-04-30 14:39:58 +0200

    fix: correction des tests sur les clics

--- tests/playlists.test.ts
+++ tests/playlists.test.ts
@@ -2,8 +2,10 @@ import request from 'supertest';
 import app from '../app';
 
 describe('SCRUM-10: Création de Playlist', () => {
+  let playlistId: number; // <-- On crée une variable pour mémoriser l'ID
+
   it('doit créer une playlist avec tous les attributs', async () => {
-    const res = await request(app).post('/playlists').send({
+    const res = await request(app).post('/playlist').send({
       name: "Ma Super Liste",
       creator: "Antoine",
       style: "Rock",
@@ -11,11 +13,16 @@ describe('SCRUM-10: Création de Playlist', () => {
     });
     expect(res.statusCode).toBe(201);
     expect(res.body.clicks).toBe(0);
+    
+    playlistId = res.body.id; // <-- On sauvegarde le véritable ID créé (ex: 4, 5...)
   });
+
   it('doit incrémenter les clics à chaque consultation', async () => {
-    const res = await request(app).get('/playlists/1');
+    // On remplace '1' par notre variable playlistId
+    const res = await request(app).get(`/playlist/${playlistId}`);
     expect(res.body.clicks).toBe(1);
-    const res2 = await request(app).get('/playlists/1');
+    
+    const res2 = await request(app).get(`/playlist/${playlistId}`);
     expect(res2.body.clicks).toBe(2);
   });
 });
